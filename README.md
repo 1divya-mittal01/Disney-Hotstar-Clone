7c77bc62042379b69195a8ae535ba20ee7d41857
Divya Mittal <92794446+1divya-mittal01@users.noreply.github.com>
2022-07-02 16:48:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+#Hostar Clone
+Images and Videos
+Star to repo is Appreciated
+This repo only contains images and videos.
---
+++ index.js
@@ -0,0 +1,120 @@
+let movies = [
+    {
+      name: "falcon and the winter soldier",
+      des:
+        "The Falcon and the Winter Soldier is an American television miniseries created by Malcolm Spellman for the streaming service Disney+, based on Marvel Comics featuring the characters Sam Wilson / Falcon and Bucky Barnes / Winter Soldier.",
+      image: "photos/slider 2.png"
+    },
+    {
+      name: "loki",
+      des:
+        "God of Mischief and brother to Thor, Loki's tricks and schemes wreak havoc across the realms. Loki, Prince of Asgard, Odinson, rightful heir of Jotunheim, and God of Mischief, is burdened with glorious purpose. His desire to be a king drives him to sow chaos in Asgard.",
+      image: "photos/slider 1.png"
+    },
+    {
+      name: "wanda vision",
+      des:
+        "WandaVision is an American television miniseries created by Jac Schaeffer for the streaming service Disney+, based on Marvel Comics featuring the characters Wanda Maximoff / Scarlet Witch and Vision.",
+      image: "photos/slider 3.png"
+    },
+    {
+      name: "raya and the last dragon",
+      des:
+        "It was released on March 5, 2021 and is the 59th animated feature in the Disney Animated Canon. The film follows Raya, a warrior princess who must find the fabled last dragon in order to save her divided home world from a ravenous plague.",
+      image: "photos/slider 4.png"
+    },
+    {
+      name: "luca",
+      des:
+        "Pixar's “Luca,” an Italian-set animated fairy tale concerning two young sea monsters exploring an unknown human world, offers the studio's hallmark visual splendor, yet fails to venture outside of safe waters.",
+      image: "photos/slider 5.png"
+    }
+  ];
+  
+  const carousel = document.querySelector(".carousel");
+  let sliders = [];
+  
+  let slideIndex = 0; // to track current slide index.
+  
+  const createSlide = () => {
+    if (slideIndex >= movies.length) {
+      slideIndex = 0;
+    }
+  
+    // creating DOM element
+    let slide = document.createElement("div");
+    var imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+  
+    // attaching all elements
+    imgElement.appendChild(document.createTextNode(""));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+  
+    // setting up image
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+  
+    // setting elements classname
+    slide.className = "slider";
+    content.className = "slide-content";
+    h1.className = "movie-title";
+    p.className = "movie-des";
+  
+    sliders.push(slide);
+  
+    if (sliders.length) {
+      sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+        30 * (sliders.length - 2)
+      }px)`;
+    }
+  };
+  
+  for (let i = 0; i < 3; i++) {
+    createSlide();
+  }
+  
+  setInterval(() => {
+    createSlide();
+  }, 3000);
+  
+  //Video Cards
+  
+  const videoCards = [...document.querySelectorAll(".video-card")];
+  
+  videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      let video = item.children[1];
+      video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+      let video = item.children[1];
+      video.pause();
+    });
+  });
+  
+  //card sliders
+  
+  let cardContainers = [...document.querySelectorAll(".card-container")];
+  let preBtns = [...document.querySelectorAll(".pre-btn")];
+  let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+  
+  cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+  
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 200;
+    });
+  
+    preBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth + 200;
+    });
+  });
